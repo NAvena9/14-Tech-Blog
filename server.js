75824a99164f14d896dcbde82a890ee879e7e745
NOat <nicolas.cedano.avena@gmail.com>
2021-08-18 01:22:52 -0500

    server

---
+++ server.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const express = require("express");
+const exphbs = require("express-handlebars");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+
+
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+    sequelize.sync({ force: false });
+})
